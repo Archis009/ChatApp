5d7b56441645cd25f951d849328c5a2dc9a2ced6
Archis009 <vedvadnere@gmail.com>
2026-03-02 04:29:35 +0530

    refactor: standardize base URL configuration

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 // axios replaces fetching API
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : `${import.meta.env.VITE_BACKEND_URL}/api`,
+    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
     withCredentials: true,
 })
